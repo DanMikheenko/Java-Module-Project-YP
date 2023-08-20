720750df4cc0e3c4099d3b0e029751bff0315d2b
Dan Mikheenko <miheenko.denis@gmail.com>
2023-08-20 22:16:09 +0300

    Fixed code formatting

--- src/main/java/Good.java
+++ src/main/java/Good.java
@@ -1,7 +1,8 @@
 public class Good {
     String name;
     double price;
-    Good(String name, double price){
+
+    Good(String name, double price) {
         this.name = name;
         this.price = price;
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int visitorCount = getVisitorCount();
-        Calculator calculator = calculateGoods(visitorCount);
+        calculateGoods(visitorCount);
     }
-    private static int getVisitorCount(){
+
+    private static int getVisitorCount() {
         int visitorCount = 0;
-        while (true){
+        while (true) {
             System.out.println("На какое количество людей разделить чек?");
             Scanner scanner = new Scanner(System.in);
             String visitorCountString = scanner.next();
-            if (isInt(visitorCountString)){
+            if (isInt(visitorCountString)) {
                 visitorCount = Integer.parseInt(visitorCountString);
             }
-            if (visitorCount == 1){
+            if (visitorCount == 1) {
                 System.out.println("Нет смысла использовать программу для 1-го человека");
             } else if (visitorCount < 1) {
                 System.out.println("Вы ввели некорректное значение");
@@ -24,37 +25,38 @@ public class Main {
         }
         return visitorCount;
     }
-    private static Calculator calculateGoods(int visitorCount){
+
+    private static Calculator calculateGoods(int visitorCount) {
         Calculator calculator = new Calculator();
-        while (true){
+        while (true) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Пожалуйста, введите название товара или команду 'завершить' чтобы закончить расчет");
             String goodName = scanner.nextLine();
-            if (goodName.equalsIgnoreCase("завершить") ){
+            if (goodName.equalsIgnoreCase("завершить")) {
                 break;
             }
             System.out.println("Пожалуйста введите цену товара");
             double goodPrice;
             String goodPriceString = scanner.next();
-            if (isDouble(goodPriceString)){
+            if (isDouble(goodPriceString)) {
                 goodPrice = Double.parseDouble(goodPriceString);
-                if (goodPrice<0){
+                if (goodPrice < 0) {
                     System.out.println("Цена товара не может быть отрицательной");
-                }
-                else {
+                } else {
                     Good good = new Good(goodName, goodPrice);
                     calculator.addGood(good, 1);
                     System.out.println("Товар был успешно добавлен");
                 }
-            }else {
+            } else {
                 System.out.println("Вы ввели некорректное значение цены");
             }
         }
         calculator.showOrder();
-        double sumToPayPerPerson = calculator.getOrderSum()/visitorCount;
+        double sumToPayPerPerson = calculator.getOrderSum() / visitorCount;
         System.out.println("Каждый должен заплатить: " + String.format("%.2f", sumToPayPerPerson) + " " + calculator.getRubleInRightFormat(sumToPayPerPerson));
         return calculator;
     }
+
     private static boolean isInt(String s) {
         try {
             Integer.parseInt(s);
